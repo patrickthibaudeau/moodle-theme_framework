c4b1200d9e975ae5d2a9b08895eb599b451dbf42
Patrick Thibaudeau <thibaud@yorku.ca>
2022-09-30 04:02:29 -0400

    Added rediresct on login and on login as

--- db/events.php
+++ db/events.php
@@ -6,4 +6,18 @@ $observers = array(
         'priority'    => 9999,
         'internal'    => false,
     ),
+    array(
+        'eventname'   => '\core\event\user_loggedin',
+        'callback'    => '\theme_framework\events::redirect_user_to_home_page',
+        'priority'    => 9999,
+        'internal'    => false,
+    ),
+    array(
+        'eventname'   => '\core\event\user_loggedinas',
+        'callback'    => '\theme_framework\events::redirect_user_to_home_page',
+        'priority'    => 9999,
+        'internal'    => false,
+    ),
+
+
 );
--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2022093000';
+$plugin->version = '2022093002';
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = '2022041900';
