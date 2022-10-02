cb9da99ecc971685e10ede3ffc68f34c50c467a6
Patrick Thibaudeau <thibaud@yorku.ca>
2022-10-02 08:46:06 -0400

    Updated colors and removed max-width 820px

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2022100101';
+$plugin->version = '2022100203';
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = '2022041900';
